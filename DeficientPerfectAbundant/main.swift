818f3380d2e30c44efa53e6a12f9e9e079b0503b
Sebastian Tyson <styson@lcs.on.ca>
2020-03-09 11:21:15 -0400

    Initial Commit

---
+++ DeficientPerfectAbundant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DeficientPerfectAbundant
+//
+//  Created by Tyson, Sebastian on 2020-03-09.
+//  Copyright © 2020 Tyson, Sebastian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
